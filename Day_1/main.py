361e87a372727cd03649a3de12864e0751bfb7bc
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-08 13:16:58 -0300

    add more  files of day 1

---
+++ Day_1/main.py
@@ -0,0 +1,5 @@
+def teste(a, b = 4, c = 6):
+    print(a, b, c)
+
+
+teste(20, c=2)
---
+++ Day_1/project_day_1/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Hello welcome to band name generator!")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city_name + " " + pet_name
+
+print("Your band name could be " + band_name)
+
